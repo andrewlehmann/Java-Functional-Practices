24fd88cf40cf63d07c99ce7253923ed486f3c367
andrewlehmann <andrew@arlehmann.com>
2017-04-10 13:22:01 -0500

    first style of functional on primes

--- Primes/primes.java
+++ Primes/primes.java
@@ -1,8 +1,25 @@
-package Primes;
-
+import java.util.stream.IntStream;
 
 public class Sample {
 	public static void main(String[] args) {
+		System.out.println(isPrime(1));
+		System.out.println(isPrime(2));
+		System.out.println(isPrime(3));
+		System.out.println(isPrime(4));
+	}
+	// imperative style
+	private static boolean isPrimeIterative(final int number) {
+		for(int i = 0; i < number; i++) {
+			if(number % i == 0) return false;
+		}
+		return number > 1;
+	}
+	// functional style
+	private static boolean isPrime(final int number) {
+		return number > 1 &&
+			IntStream.range(2, number)
+				.noneMatch(index -> number % index == 0);
+	}
 
 	}
 }
