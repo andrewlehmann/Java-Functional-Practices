5875ffa8526ab47270f56a2b66ab8eb104193b1e
andrewlehmann <andrew@arlehmann.com>
2017-04-12 15:21:07 -0500

    on page 53

--- StringsComparatorsFilters/src/com/StringsComparatorsFilters/Compare.java
+++ StringsComparatorsFilters/src/com/StringsComparatorsFilters/Compare.java
@@ -5,10 +5,18 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 
+import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
 
 public class Compare {
+
+    public final static List<Person> people = Arrays.asList(
+            new Person("John", 20),
+            new Person("Sara", 21),
+            new Person("Jane", 21),
+            new Person("Greg", 35)
+    );
 
     private static void printPeople(final String message,
                                     final List<Person> people) {
@@ -20,19 +28,13 @@ public class Compare {
 
     public static void main(String[] args) {
 
-        final List<Person> people = Arrays.asList(
-                new Person("John", 20),
-                new Person("Sara", 21),
-                new Person("Jane", 21),
-                new Person("Greg", 35)
-        );
         List<Person> ascendingAge =
                 people.stream()
                       .sorted(Person::ageDiff)
                       .collect(toList());
         people.forEach(System.out::println);
 
-        Comparator<Person> compareAscending = Person::ageDiff;
+        Comparator<Person> compareAscending  = Person::ageDiff;
         Comparator<Person> compareDescending = compareAscending.reversed();
         printPeople("Sorted in ascending order by age: ",
                     people.stream()
@@ -47,19 +49,20 @@ public class Compare {
         );
         printPeople("Sorted by name: ",
                     people.stream()
-                          .sorted(Comparator.comparing(Person::getName));
+                          .sorted(Comparator.comparing(Person::getName))
+                          .collect(toList())
+        );
 
         people.stream()
               .min(Person::ageDiff)
-              .ifPresent(youngest -> System.out.println("Youngest: " + youngest)
-              );
+              .ifPresent(youngest -> System.out.println("Youngest: " + youngest));
 
-        final Function<Person, Integer> byAge = Person::getAge;
-        final Function<Person, String> byName = Person::getName;
+        final Function<Person, Integer> byAge  = Person::getAge;
+        final Function<Person, String>  byName = Person::getName;
 
         printPeople("Sorted in ascending order by age then name: ",
                     people.stream()
-                          .sorted(Comparator.comparing(byAge).thenComparing(byName))
+                          .sorted(comparing(byAge).thenComparing(byName))
                           .collect(toList())
         );
 
--- StringsComparatorsFilters/src/com/StringsComparatorsFilters/OlderThan20.java
+++ StringsComparatorsFilters/src/com/StringsComparatorsFilters/OlderThan20.java
@@ -1,8 +1,23 @@
 package com.StringsComparatorsFilters;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.StringsComparatorsFilters.Compare.people;
+
+
 /**
  * Created by root on 4/12/17.
  */
 public class OlderThan20 {
-    
+    public static void main(String[] args) {
+        List<Person> olderThan20 = new ArrayList<>();
+        people.stream()
+              .filter(person -> person.getAge() > 20)
+              .forEach(person -> olderThan20.add(person));
+        System.out.println("olderThan20 = " + olderThan20);
+
+
+    }
+
 }
